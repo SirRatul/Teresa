60bf686c9931b13f7bc516cff90cbfd91fe26cd2
Ayon <ayonmahmud53@gmail.com>
2020-06-05 22:51:12 +0600

    Added Process Section in Home page

---
+++ src/HomePage/components/Process.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import ProcessPhoto from "../../shared/img/Process.jpg";
+import "./Process.css";
+import { components } from "react-select";
+
+class Process extends Component {
+  render() {
+    return (
+      <div
+        className="container-fluid process-section"
+        style={{ backgroundColor: "#FBF8F4" }}
+      >
+        <img className="process-image" src={ProcessPhoto} alt="process" />
+      </div>
+    );
+  }
+}
+
+export default Process;
